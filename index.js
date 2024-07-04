7f9d3b766d0c0afe94fc7a6d85b5e93b8569e96c
Lucas Barrena <lucasbarrena98@gmail.com>
2024-07-04 19:15:08 -0300

    Add `--build-arg`

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const minimist = require('minimist')
 module.exports = function dockerBin (dirname, opts = {}) {
   const argv = minimist(process.argv.slice(2), {
     boolean: ['sudo', 'persistent', 'cwd', 'home', 'privileged'],
-    string: ['device', 'volume'],
+    string: ['device', 'volume', 'build-arg'],
     default: {
       sudo: opts.sudo,
       persistent: opts.persistent,
@@ -14,7 +14,8 @@ module.exports = function dockerBin (dirname, opts = {}) {
       home: opts.home,
       volume: opts.volume,
       privileged: opts.privileged,
-      device: opts.device
+      device: opts.device,
+      'build-arg': opts.buildArg
     },
     alias: {
       v: 'volume',
@@ -34,17 +35,21 @@ module.exports = function dockerBin (dirname, opts = {}) {
     device = null
   } = { ...opts, ...argv }
 
+  const buildArg = argv['build-arg'] || opts.buildArg
+
   const pkg = require(path.join(dirname, 'package.json'))
   const tag = pkg.name + ':npm-' + pkg.version
 
   try {
     const once = !exec('docker', ['images', tag, '-q', '--no-trunc'], { sudo }).length
 
+    const buildArgs = argsPush([], '--build-arg', buildArg)
+
     if (once) {
-      exec('docker', ['build', '--progress=plain', '--tag=' + tag, dirname], { sudo, stdio: 'inherit' })
+      exec('docker', ['build', ...buildArgs, '--tag=' + tag, '--progress=plain', dirname], { sudo, stdio: 'inherit' })
     }
 
-    const imageId = exec('docker', ['build', '--tag=' + tag, '-q', dirname], { sudo }).toString().trim()
+    const imageId = exec('docker', ['build', ...buildArgs, '--tag=' + tag, '-q', dirname], { sudo }).toString().trim()
 
     const args = ['run', '-ti']
 
@@ -52,13 +57,7 @@ module.exports = function dockerBin (dirname, opts = {}) {
 
     if (cwd) args.push('-v=' + path.resolve('.') + ':/mnt/cwd')
     if (home) args.push('-v=' + os.homedir() + ':/mnt/home')
-    if (volume) {
-      const volumes = Array.isArray(volume) ? volume : [volume]
-
-      for (const v of volumes) {
-        args.push('-v=' + v)
-      }
-    }
+    if (volume) argsPush(args, '-v', volume)
 
     if (privileged) args.push('--privileged')
     if (device) args.push('--device=' + device)
@@ -98,4 +97,14 @@ function parseVariadic (argv) {
   }
 
   return argv.splice(index + 1)
+}
+
+function argsPush (args, key, value) {
+  const arr = Array.isArray(value) ? value : [value]
+
+  for (const v of arr) {
+    args.push(key + '=' + v)
+  }
+
+  return args
 }
